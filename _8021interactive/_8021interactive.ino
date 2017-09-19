4ca09e6761b1b57a767fc60c5e9cc9067775c293
davidmorancc <david@commdat.com>
2017-09-19 03:25:03 -0400

    auto ranging updates,work on breathing effect,better scaling

--- _8021interactive/_8021interactive.ino
+++ _8021interactive/_8021interactive.ino
@@ -2,14 +2,17 @@
 #include "ESP8266WiFi.h"
 #include <WiFiUdp.h>
 
-const char* ssid        = "commdat";
+const char* ssid        = "DavidsiPhone";
 const char* password    = "0p3nm35h";
+const int LEDS          = 3;
 int rssi_max            = 0;
 int rssi_min            = 0;
 
-int auto_rssi_size      = 100;
-int auto_rssi[100];
-int auto_rssi_padding   = 6;
+//both these need to be set to the same size
+int auto_rssi_size      = 20;
+int auto_rssi[20];
+
+int auto_rssi_padding   = 1;
 int rssi_previous       = 0;
 int auto_rssi_pointer   = 1;
 
@@ -22,7 +25,7 @@ int auto_rssi_pointer   = 1;
 //   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
 //   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
 //   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(2, PIN, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(LEDS, PIN, NEO_GRB + NEO_KHZ800);
 
 WiFiUDP Udp;
 
@@ -71,22 +74,22 @@ void strip_test(int number_leds) {
 void breath() {
   //Written by: Jason Yandell
   
-  int TOTAL_LEDS = 2;
-  float MinimumBrightness = 100;
+  int TOTAL_LEDS = 3;
+  float MinimumBrightness = 50;
   float MaximumBrightness = 200;
-  float SpeedFactor = 0.007; // I don't actually know what would look good
-  float StepDelay = 2; // ms for a step delay on the lights
+  float SpeedFactor = 0.032; // I don't actually know what would look good
+  float StepDelay = 1; // ms for a step delay on the lights
   
   // Make the lights breathe
-  for (int i = 0; i < 65535; i++) {
+  for (int i = 0; i < ((2*PI)/SpeedFactor); i++) {
    
     // Intensity will go from 10 - MaximumBrightness in a "breathing" manner
     float intensity = MaximumBrightness /2.0 * (1.0 + sin(SpeedFactor * i));
     strip.setBrightness(intensity);
-
+    
     // Now set every LED to that color
     for (int ledNumber=0; ledNumber<TOTAL_LEDS; ledNumber++) {
-      strip.setPixelColor(ledNumber, 180, 0, 255);
+      strip.setPixelColor(ledNumber, 70, 70, 255);
     }
   
     strip.show();
@@ -119,21 +122,57 @@ void loop() {
   
   //get the strenght of the current network
   rssi_in = get_rssi();
-  rssi = convert_rssi(rssi_in);
   
   //only update the strip if there's a change
-  if (rssi != rssi_previous) {
-    //strip.setBrightness(rssi);
-    strip.setPixelColor(0, rssi, 0, rssi);
-    strip.setPixelColor(1, rssi, 0, rssi);
-    
-    strip.show(); 
+  if (rssi_in != rssi_previous) {
 
-    rssi_previous = rssi;
+    //convert and autoscale the rssi
+    rssi = convert_rssi(rssi_in); 
+    
+    if (rssi <= 10) {
+      for (int i = 0; i < 5; i++) {
+        strip.setPixelColor(1, 255, 0, 0);
+        strip.setPixelColor(0, 255, 0, 0);
+        strip.setPixelColor(2, 255, 0, 0);     
+        strip.show(); 
+        delay(50);
+        strip.setPixelColor(1, 100, 0, 0);
+        strip.setPixelColor(0, 100, 0, 0);
+        strip.setPixelColor(2, 100, 0, 0);
+        strip.show(); 
+        delay(50);
+        strip.setPixelColor(1, 20, 0, 0);
+        strip.setPixelColor(0, 20, 0, 0);
+        strip.setPixelColor(2, 20, 0, 0);
+        strip.show(); 
+        delay(50);
+      }
+    } else if (rssi >= 245) {
+      for (int i = 0; i < 5; i++) {
+        strip.setPixelColor(0, 200, 200, 0);
+        strip.setPixelColor(2, 20, 20, 0);
+        strip.show(); 
+        delay(50);
+        strip.setPixelColor(0, 20, 20, 0);
+        strip.setPixelColor(2, 200, 200, 0);
+        strip.show(); 
+        delay(50);
+      }
+    } 
+
+    //breath(); 
+    strip.setPixelColor(0, 70, 70, 255);
+    strip.setPixelColor(1, 70, 70, 255);
+    strip.setPixelColor(2, 70, 70, 255);
+    strip.show();
+    
+    rssi_previous = rssi_in;
+
+  
     /* Testing */
     Serial.println("Min/RSSI/Max//Convert " + String(rssi_min) + "/" +  String(abs(rssi_in)) + "/" + String(rssi_max) + "//" +  String(rssi));   
   }
-  delay(150);
+  
 }
 
 //Converts rssi from a neg number, runs the auto_rssi_minmax, reverses the direction, scales on 0-255 
@@ -146,6 +185,13 @@ int convert_rssi(int rssi_in) {
   if (rssi_min != rssi_max) {
     //map function to scale the input to 0-255
     rssi_out = map(rssi_in, rssi_min, rssi_max, 0, 255);
+  }
+
+  //make sure it doesn't scale past the bounds since we can turn scaling off
+  if (rssi_in > rssi_max) {
+    rssi_out = 255;
+  } else if (rssi_min > rssi_in) {
+    rssi_out = 0;
   }
 
   //reverse the range
@@ -194,11 +240,11 @@ void auto_rssi_minmax(int rssi_in) {
   rssi_max = get_maximum(auto_rssi);
 
   //pads the min and max, gives a less sudden effects
-  if ((rssi_max-rssi_min)<auto_rssi_padding) {
+  /*if ((rssi_max-rssi_min)<auto_rssi_padding) {
     rssi_diff = (auto_rssi_padding-(rssi_max-rssi_min))/2;
     rssi_min = rssi_min - rssi_diff;
     rssi_max = rssi_max + rssi_diff;
-  }
+  }*/
 
   //Increment to pointer and reset once we get to top of the array
   if (auto_rssi_pointer == auto_rssi_size) { 
